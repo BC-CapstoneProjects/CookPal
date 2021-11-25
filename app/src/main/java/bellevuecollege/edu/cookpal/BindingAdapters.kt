49dc9b1d6aae615a18eaa3945af8a8ae4f5e6e76
trang2020 <tranthutrang1985@gmail.com>
2021-11-24 22:44:48 -0800

    Display dish images and titles

--- app/src/main/java/bellevuecollege/edu/cookpal/BindingAdapters.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/BindingAdapters.kt
@@ -1,13 +1,56 @@
 package bellevuecollege.edu.cookpal
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import bellevuecollege.edu.cookpal.network.Recipe
 import bellevuecollege.edu.cookpal.recipes.IngredientSearchApiStatus
 import bellevuecollege.edu.cookpal.recipes.RecipeGridAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
+@BindingAdapter("recipeImage")
+fun bindRecipeImage(imgView: ImageView, recipe: Recipe?) {
+    recipe?.let {
+        val imgUri = recipe.imgSrcUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    recipe.isLoadedSuccessful = false
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    recipe.isLoadedSuccessful = true
+                    return false
+                }
+            })
+            .into(imgView)
+    }
+}
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView,
