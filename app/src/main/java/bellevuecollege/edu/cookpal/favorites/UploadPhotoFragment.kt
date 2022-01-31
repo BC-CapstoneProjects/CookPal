067b4bd126000afb4000020a7351abbeea1c11c8
Elliot Weimann <elliot.weimann@bellevuecollege.edu>
2022-01-31 11:36:33 -0800

    changed fragment name for UploadPhotoFragment class

--- app/src/main/java/bellevuecollege/edu/cookpal/favorites/UploadPhotoFragment.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/favorites/UploadPhotoFragment.kt
@@ -35,7 +35,7 @@ class UploadPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_photo, container, false)
+        return inflater.inflate(R.layout.fragment_capture_photo, container, false)
     }
 
     companion object {
