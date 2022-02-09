1ddd4bcdbf3f66a9d4e2c168367d95bade2d021f
Elliot Weimann <elliot.weimann@bellevuecollege.edu>
2022-02-09 13:39:37 -0800

    added basic sign up and login with firebase

--- app/src/main/java/bellevuecollege/edu/cookpal/profile/loginFragment.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/profile/loginFragment.kt
@@ -1,14 +1,25 @@
 package bellevuecollege.edu.cookpal.profile
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import bellevuecollege.edu.cookpal.R
 import bellevuecollege.edu.cookpal.databinding.FragmentLoginBinding
+import bellevuecollege.edu.cookpal.home_screen.HomeScreenFragment
 import bellevuecollege.edu.cookpal.home_screen.HomeScreenViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +32,8 @@ class LoginFragment : Fragment() {
         fun newInstance() = LoginFragment()
     }
 
+    private lateinit var auth: FirebaseAuth
+    private var TAG = "EmailPassword"
     private lateinit var viewModel: LoginFragmentViewModel
 
     override fun onCreateView(
@@ -31,19 +44,88 @@ class LoginFragment : Fragment() {
         val binding = FragmentLoginBinding.inflate(inflater)
 
         binding.lifecycleOwner = this
+        auth = Firebase.auth
 
         //button listener for loginFragment to signUpFragment
         binding.signUpButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+
+            var emailAdd : String = binding.emailAddress.text.toString()
+            var pwd : String = binding.password.text.toString()
+            createAccount(emailAdd, pwd)
+          //  view.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
         }
 
         //button listener for loginFragment to profileFragment
         binding.loginButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+
+            var emailAdd : String = binding.emailAddress.text.toString()
+            var pwd : String = binding.password.text.toString()
+
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(requireView().getWindowToken(), 0)
+
+            binding.emailAddress.setText("")
+            binding.password.setText("")
+
+            signIn(emailAdd, pwd)
+            //view.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
         }
 
         // Inflate the layout for this fragment
         return binding.root
     }
 
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+
+        var at : Activity = getContext() as Activity;
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(at, { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(getActivity(), "signIn Authentication success.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(getActivity(), "signIn Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            })
+        // [END sign_in_with_email]
+    }
+
+    private fun createAccount(email: String, password: String) {
+
+        var at : Activity = getContext() as Activity;
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(at) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(getActivity(), "createAccount Authentication success.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(getActivity(), "createAccount Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
 }
