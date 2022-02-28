b0e2c4db69560a70ec11d9bd52dc33e57c2d4c03
Elliot Weimann <elliot.weimann@bellevuecollege.edu>
2022-02-28 12:03:43 -0800

    added name and emailaddress to profile object

--- app/src/main/java/bellevuecollege/edu/cookpal/profile/UserProfile.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/profile/UserProfile.kt
@@ -13,25 +13,34 @@ class UserProfile {
         setProfile(mp)
     }
 
-    fun setProfile(mp :Map<String, String>?)
-    {
-        if (mp == null)
-        {
+    fun setProfile(mp :Map<String, String>?) {
+        if (mp == null) {
             return;
         }
 
         try {
             val favoriterecipes = mp.get("favoriterecipes") as ArrayList<Recipe>
 
-            for ( value in favoriterecipes) {
+            for (value in favoriterecipes) {
                 favoriteRecipes.add(value)
             }
+        } catch (e: Exception) {
+
         }
-        catch (e : Exception)
-        {
+
+        try {
+            emailAddress = mp.get("emailAddress") as String
+        } catch (e: Exception) {
+
+        }
+
+        try {
+            name = mp.get("name") as String
+        } catch (e: Exception) {
 
         }
     }
-
+    var name : String = "test"
+    var emailAddress : String = "testem"
     val favoriteRecipes : ArrayList<Recipe> = ArrayList<Recipe>()
 }
