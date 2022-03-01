c44d5839bd39cc9bf1053d778a6bca72153e69e7
Elliot Weimann <elliot.weimann@bellevuecollege.edu>
2022-03-01 15:29:44 -0800

    allow user to upload a profile photo

--- app/src/main/java/bellevuecollege/edu/cookpal/profile/ProfileFragment.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/profile/ProfileFragment.kt
@@ -1,25 +1,36 @@
 package bellevuecollege.edu.cookpal.profile
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import bellevuecollege.edu.cookpal.databinding.FragmentProfileBinding
 import bellevuecollege.edu.cookpal.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_profile.*
-
+import com.google.firebase.storage.FirebaseStorage
 
 /**
  * A simple [Fragment] subclass.
@@ -34,33 +45,14 @@ class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileFragmentViewModel
     private val up:UserProfile = UserProfile()
-
-    val REQUEST_IMAGE_CAPTURE = 1
-
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView3.setImageBitmap(imageBitmap)
-val dt = data.data
-var t = 0
-        }
-    }
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val binding = FragmentProfileBinding.inflate(inflater)
+          binding = FragmentProfileBinding.inflate(inflater)
 
         binding.lifecycleOwner = this
 
@@ -79,7 +71,12 @@ var t = 0
         }
 
         binding.takepicProfile.setOnClickListener { view: View ->
-            dispatchTakePictureIntent()
+            val intent = Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+            }
+            choosePictureFromGallery.launch(intent)
+
         }
 
         var fbu : FirebaseUser? = FirebaseAuth.getInstance().getCurrentUser()
@@ -93,6 +90,10 @@ var t = 0
 
             binding.name.setText(up.name)
             binding.emailAddress.setText(up.emailAddress)
+
+            if (up.profilePhotoPath != ""){
+               DownloadImageFromInternet(binding.imageView3).execute( up.profilePhotoPath)
+            }
         }
 
         binding.updateProfile.setOnClickListener{ view : View ->
@@ -118,9 +119,86 @@ var t = 0
             view.findNavController().navigate(R.id.action_profileFragment_to_selectVoiceFragment)
         }
 
-
-        // Inflate the layout for this fragment
+         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private lateinit var filePath: Uri
+
+    private val choosePictureFromGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                filePath = it.data?.data!!
+
+                var str = filePath.toString()
+
+
+                try {
+                    filePath?.let {
+                        if(Build.VERSION.SDK_INT < 28) {
+                            val bitmap = MediaStore.Images.Media.getBitmap(
+                                this.activity?.contentResolver,
+                                filePath
+                            )
+                            binding.imageView3.setImageBitmap(bitmap)
+                        } else {
+                            val source = this.activity?.let { it1 -> ImageDecoder.createSource(it1.contentResolver, filePath) }
+                            val bitmap = source?.let { it1 -> ImageDecoder.decodeBitmap(it1) }
+                            binding.imageView3.setImageBitmap(bitmap)
+                        }
+                        uploadFileToFirebaseStorage()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                Log.d(TAG, "Fail to select image from Gallery")
+            }
+        }
+
+    private fun uploadFileToFirebaseStorage() {
+        if (filePath == null) return
+
+        var fbu : FirebaseUser? = FirebaseAuth.getInstance().getCurrentUser()
+
+        val filename = fbu?.uid
+        val ref = FirebaseStorage.getInstance().getReference("/profile_photo/$filename")
+
+        ref.putFile(filePath!!)
+            .addOnSuccessListener {
+                Log.d(TAG, "Successfully uploaded image: ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d(TAG, "File Location: $it")
+
+                    up.profilePhotoPath = it.toString()
+                    UserProfileHelper.saveProfile(up)
+                }
+            }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @Suppress("DEPRECATION")
+    private inner class DownloadImageFromInternet(var imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+        init {
+            Toast.makeText(getActivity(), "Please wait, it may take a few minute...",     Toast.LENGTH_SHORT).show()
+        }
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val imageURL = urls[0]
+            var image: Bitmap? = null
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+            }
+            catch (e: Exception) {
+                Log.e("Error Message", e.message.toString())
+                e.printStackTrace()
+            }
+            return image
+        }
+        override fun onPostExecute(result: Bitmap?) {
+            imageView.setImageBitmap(result)
+        }
     }
 
 }
