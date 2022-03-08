30af832b8bb4e1889e6e4e4511c53d231e168d55
David Vandenbrouck <39681796+DavePie@users.noreply.github.com>
2022-03-07 19:05:42 -0800

    Upload recipes functionality (#105)
    Uploads at app launch. Utilizes app for authentication purposes.

---
+++ app/src/main/java/bellevuecollege/edu/cookpal/network/RecipeData.kt
@@ -0,0 +1,13 @@
+package bellevuecollege.edu.cookpal.network
+
+class RecipeData {
+    var title: String = ""
+    var steps: ArrayList<String> = arrayListOf()
+    var imgUrl: String = ""
+    var sourceUrl: String = ""
+    var ingredients: ArrayList<String> = arrayListOf()
+    var reviewNumber: Number = 0
+    var rating: String = ""
+    var totalTime: String = ""
+
+}
