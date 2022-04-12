98f5031d2681e6b838b02ec22d400a7a47af4352
Elliot Weimann <ebitar2006@aol.com>
2022-04-12 15:31:57 -0700

    get environment server url from config file

---
+++ app/src/main/java/bellevuecollege/edu/cookpal/network/ConfigEnv.kt
@@ -0,0 +1,6 @@
+package bellevuecollege.edu.cookpal.network
+
+data class ConfigEnv (
+    val localserver:String = "",
+    val awsserver:String = ""
+)
--- app/src/main/java/bellevuecollege/edu/cookpal/network/DownloadRecipesMongoDB.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/network/DownloadRecipesMongoDB.kt
@@ -1,10 +1,13 @@
 package bellevuecollege.edu.cookpal.network
 
+import Config
 import RecipeAdapter
 import SearchParam
 import SearchType
 import android.content.Context
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -12,8 +15,13 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.Response
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class DownloadRecipesMongoDB {
+
+    private lateinit var configenv: ConfigEnv
+    private lateinit var config: Config
 
     suspend fun getRecipesout(keyWord: String, context: Context): List<Recipe> {
         try
@@ -40,21 +48,29 @@ class DownloadRecipesMongoDB {
         val client = OkHttpClient()
         val mediaType = "application/json".toMediaTypeOrNull()
 
-        val request = Request.Builder()
-                .url("http://10.0.0.167:3000/api/recipe/title/" + keyWord)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Access-Control-Request-Headers", "*")
-                .build()
-        val response = withContext(Dispatchers.IO) {
-            client.newCall(request).execute()
 
-        }
-        //logger.info { "Response code: ${response.code()}" }
-        val responseBody = response.body
-        println("this runs" )
 
         try
         {
+
+            val inputStream = context.assets.open("configenv.yaml")
+            val mapper = ObjectMapper(YAMLFactory())
+            configenv = mapper.readValue(inputStream, ConfigEnv::class.java)
+
+
+            val request = Request.Builder()
+                    .url(configenv.localserver + "/api/recipe/title/" + keyWord)
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Access-Control-Request-Headers", "*")
+                    .build()
+            val response = withContext(Dispatchers.IO) {
+                client.newCall(request).execute()
+
+            }
+            //logger.info { "Response code: ${response.code()}" }
+            val responseBody = response.body
+            println("this runs" )
+
             val responseString = withContext(Dispatchers.IO) {
                 responseBody?.string()
             }
