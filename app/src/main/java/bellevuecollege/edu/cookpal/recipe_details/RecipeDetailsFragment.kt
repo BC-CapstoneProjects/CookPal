9079e494d4c1420f97e9f5d51d79fd271a1ef750
TonnyAn <tonny.an@bellevuecollege.edu>
2022-04-12 17:52:57 -0700

    Added functionality to spinner

--- app/src/main/java/bellevuecollege/edu/cookpal/recipe_details/RecipeDetailsFragment.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/recipe_details/RecipeDetailsFragment.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -24,9 +25,8 @@ import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
 import java.io.File
 import java.util.*
-import android.widget.Spinner
 
-class RecipeDetailsFragment : Fragment() {
+class RecipeDetailsFragment : Fragment(), AdapterView.OnItemSelectedListener {
     private val UTTERANCE_ID: String = "tts1"
     private lateinit var mTTS: TextToSpeech
     private lateinit var recipeVoiceFile: File
@@ -34,49 +34,32 @@ class RecipeDetailsFragment : Fragment() {
     private lateinit var binding: RecipeDetailsFragmentBinding
     private val up: UserProfile = UserProfile()
     private lateinit var recipe : Recipe
-    private var modelDownloaded: Boolean = false //flag to check if translator model has been downloaded
-    private lateinit var engJapTranslator: Translator
-    private val espan: Locale = Locale("es","es")
+    private lateinit var textTranslator: Translator
+    private val espanol: Locale = Locale("es","es")
     private val lang: Array<String> = arrayOf("English", "Spanish", "Japanese")
+    private lateinit var instructions: String
+    private lateinit var translatedText: String
+    private var flag: Int = -1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    /**
+     * Can remove onCreate function
+     * spinner is default to english so
+     * TTS engine is created there.
+     * See OnItemSelected function down below.
+     */
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
         // Setup Text To Speech engine
-        mTTS = TextToSpeech(
-            activity?.applicationContext
-        ) { status ->
-            if (status != TextToSpeech.ERROR) {
-                //if there is no error then set language
-                //mTTS.language = Locale.JAPANESE
-                mTTS.language = espan
-            }
-        }
-        /**
-         * Set up translator model
-         */
-        val options = TranslatorOptions.Builder()
-            .setSourceLanguage(TranslateLanguage.ENGLISH)
-            .setTargetLanguage(TranslateLanguage.SPANISH)
-            .build()
-        engJapTranslator = Translation.getClient(options)
-        var conditions = DownloadConditions.Builder()
-            .requireWifi()
-            .build()
-        //download language model
-        //Language models are around 30MB, don't have too many.
-        engJapTranslator.downloadModelIfNeeded(conditions)
-            .addOnSuccessListener {
-                // Model downloaded successfully. Okay to start translating
-                // Set a flag, unhide the translation UI, etc.
-                Log.d("Translator", "Model download successful")
-                modelDownloaded = true
-            }
-            .addOnFailureListener {
-                //Model couldn't be downloaded or other internal error
-                Log.e("Translator", "Model download failed")
-            }
-
-    }
+        // Defaults to english
+//        mTTS = TextToSpeech(
+//            activity?.applicationContext
+//        ) { status ->
+//            if (status != TextToSpeech.ERROR) {
+//                //if there is no error then set language
+//                mTTS.language = Locale.US
+//            }
+//        }
+    //}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -107,14 +90,16 @@ class RecipeDetailsFragment : Fragment() {
                 binding.recipeIngredients.text =
                     parsedRecipe.ingredients.joinToString("") { "- $it\n" }
                 binding.recipeInstructions.text = parsedRecipe.steps.mapIndexed{index, s -> "${index+1}) $s" }.joinToString("") { "$it\n" }
+                instructions = recipe.steps.mapIndexed{index, s -> "${index+1}) $s" }.joinToString("") { "$it\n" }
+                translatedText = instructions
             }
         }
-
         //Array adapter for spinner/drop down menu
         val aa = ArrayAdapter(requireActivity().applicationContext, R.layout.simple_spinner_item, lang)
         aa.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
         val spinner = binding.langSpinner
         spinner.adapter = aa
+        spinner.onItemSelectedListener = this
 
         binding.addFavorite.setOnClickListener {
 
@@ -124,48 +109,17 @@ class RecipeDetailsFragment : Fragment() {
         // Setup Speak button handler
         binding.speakRecipeInstructionsButton.setOnClickListener {
             if (tempView != null) {
-                val instructions = recipe.steps.mapIndexed{index, s -> "${index+1}) $s" }.joinToString("") { "$it\n" }
-
-                if (instructions != null) {
-                    if (modelDownloaded) {
-                        //translate english instructions to language
-                        engJapTranslator.translate(instructions)
-                            .addOnSuccessListener {
-                                Log.d("Translated text", it) //it refers to the translated string
-                                mTTS.speak(it, TextToSpeech.QUEUE_ADD, null, UTTERANCE_ID)
-                                binding.pauseRecipeInstructionsButton.isEnabled = true
-                                Log.d("Recipe Details Fragment", "TTS successfully speak out recipe")
-                            }
-                            .addOnFailureListener {
-                                Log.e("Translated text", "Failed to translate")
-                            }
-                    }
-                } else {
+                if (translatedText != null) {
+                    mTTS.speak(translatedText, TextToSpeech.QUEUE_ADD, null, UTTERANCE_ID)
+                }
+                else {
                     Log.e(
                         "Recipe Details Fragment",
                         "No recipe instructions supplied for TTS"
                     )
                 }
-
-                /**
-                 * Old TTS code
-                 */
-//                Log.d("-----instructions-----", instructions)
-//                if (instructions != null) {
-//                    if (instructions.isNotEmpty()) {
-//                        mTTS.speak(instructions, TextToSpeech.QUEUE_ADD, null, UTTERANCE_ID)
-//                        binding.pauseRecipeInstructionsButton.isEnabled = true
-//                        Log.d("Recipe Details Fragment", "TTS successfully speak out recipe")
-//                    } else {
-//                        Log.e(
-//                            "Recipe Details Fragment",
-//                            "No recipe instructions supplied for TTS"
-//                        )
-//                    }
-//                }
             }
         }
-
         // Setup Pause button handler
         binding.pauseRecipeInstructionsButton.setOnClickListener {
             if (mTTS.isSpeaking) {
@@ -183,6 +137,113 @@ class RecipeDetailsFragment : Fragment() {
             }
         }
         return binding.root
+    }
+
+    /**
+     * Spinner drop down listeners
+     */
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position) {
+            0 -> {
+                //English set up
+                mTTS = TextToSpeech(
+                    activity?.applicationContext
+                ) { status ->
+                    if (status != TextToSpeech.ERROR) {
+                        //if there is no error then set language
+                        mTTS.language = Locale.US
+                    }
+                }
+                flag+=1
+                if(flag > 0) {
+                    translatedText = instructions
+                }
+            }
+            1 -> {
+                //Set up text to speech language
+                mTTS = TextToSpeech(
+                    activity?.applicationContext
+                ) { status ->
+                    if (status != TextToSpeech.ERROR) {
+                        //if there is no error then set language
+                        mTTS.language = espanol
+                    }
+                }
+
+                /**
+                 * Set up spanish translator model
+                 */
+                val options = TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.SPANISH)
+                    .build()
+                textTranslator = Translation.getClient(options)
+                var conditions = DownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                //download language model
+                //Language models are around 30MB, don't have too many.
+                textTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        // Model downloaded successfully. Okay to start translating
+                        // Set a flag, unhide the translation UI, etc.
+                        Log.d("Translator", "Model download successful")
+                        textTranslator.translate(instructions)
+                            .addOnSuccessListener {
+                                Log.d("Translated text", it) //it refers to the translated string
+                                translatedText = it
+                            }
+                    }
+                    .addOnFailureListener {
+                        //Model couldn't be downloaded or other internal error
+                        Log.e("Translator", "Model download failed")
+                    }
+            }
+            2 -> {
+                //Set up TTS language
+                mTTS = TextToSpeech(
+                    activity?.applicationContext
+                ) { status ->
+                    if (status != TextToSpeech.ERROR) {
+                        //if there is no error then set language
+                        mTTS.language = Locale.JAPANESE
+                    }
+                }
+
+                /**
+                 * Set up japanese translator model
+                 */
+                val options = TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.JAPANESE)
+                    .build()
+                textTranslator = Translation.getClient(options)
+                var conditions = DownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                //download language model
+                //Language models are around 30MB, don't have too many.
+                textTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        // Model downloaded successfully. Okay to start translating
+                        // Set a flag, unhide the translation UI, etc.
+                        Log.d("Translator", "Model download successful")
+                        textTranslator.translate(instructions)
+                            .addOnSuccessListener {
+                                Log.d("Translated text", it) //it refers to the translated string
+                                translatedText = it
+                            }
+                    }
+                    .addOnFailureListener {
+                        //Model couldn't be downloaded or other internal error
+                        Log.e("Translator", "Model download failed")
+                    }
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
     }
 
     override fun onPause() {
