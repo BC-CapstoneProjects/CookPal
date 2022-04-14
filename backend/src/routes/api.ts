ae7e19fd69e73fb03be91173a93ede756bfb0677
Elliot Weimann <ebitar2006@aol.com>
2022-04-14 09:45:52 -0700

    added api for testing to see if the web server works

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,11 +1,17 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import recipeRouter from './recipe-router';
-
+import StatusCodes from 'http-status-codes';
+ 
 // Export the base-router
 const baseRouter = Router();
-
+const { OK } = StatusCodes;
 // Setup routers
 baseRouter.use('/recipe', recipeRouter);
+
+baseRouter.get('/test', async (req: Request, res: Response) => {
+     
+    return res.status(OK).json({'data':'some data'});
+});
 
 // Export default.
 export default baseRouter;
