2675ed19c8aedeba7a3fb39be87c2d99345ec536
Elliot Weimann <ebitar2006@aol.com>
2022-04-15 23:46:56 -0700

    use https in production when running server on aws for better securit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,32 @@ import server from './server';
 const serverStartMsg = 'Express server started on port: ',
         port = (process.env.PORT || 3000);
 
-// Start server
-server.listen(port, () => {
-    logger.info(serverStartMsg + port);
-});
+// use https in production when running server on aws for better security 
+if (process.env.NODE_ENV === 'production') {
+    const fs = require('fs'); 
+    const https = require('https'); 
+
+    // Certificate
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/cookpal.cloud/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/cookpal.cloud/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/cookpal.cloud/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    const httpsServer = https.createServer(credentials, server);
+
+    // Start server
+    httpsServer.listen(port, () => {
+        console.log('HTTPS Server running on port ' + port);
+    });
+}
+else {
+    // Start server
+    server.listen(port, () => {
+        logger.info(serverStartMsg + port);
+    });  
+}
