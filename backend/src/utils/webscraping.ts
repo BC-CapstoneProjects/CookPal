b7ac28b6a11c3c0fc556170eabe5875c232b81b6
Elliot Weimann <ebitar2006@aol.com>
2022-05-04 15:03:39 -0700

    added maxResults parameter to getResults

--- backend/src/utils/webscraping.ts
+++ backend/src/utils/webscraping.ts
@@ -1,7 +1,10 @@
 import { IRecipe } from "@models/recipe-model";
 
 class webscraping {
-  public static async getResults(title: string): Promise<Array<IRecipe>> {
+  public static async getResults(
+    title: string,
+    maxResults: number = -1
+  ): Promise<Array<IRecipe>> {
     return new Promise(function (resolve, reject) {
       resolve([]);
     });
