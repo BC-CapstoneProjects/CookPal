3e8e12a1abb6a17f14ec8f46cf2537cd89b38b87
Elliot Weimann <ebitar2006@aol.com>
2022-05-04 14:40:24 -0700

    fixed coding error

--- backend/src/utils/webscraping.ts
+++ backend/src/utils/webscraping.ts
@@ -1,7 +1,9 @@
 import { IRecipe } from "@models/recipe-model";
 
 class webscraping {
-  public static getResults(title: string): Array<IRecipe> {}
+  public static getResults(title: string): Array<IRecipe> {
+    return [];
+  }
 }
 
 export default webscraping;
