88abddda44c435eb0dff928deb966b61834d59c4
TonnyAn <tonny.an@bellevuecollege.edu>
2022-05-18 10:10:55 -0700

    Setting up favorite recipes xml

--- app/src/main/java/bellevuecollege/edu/cookpal/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/favorites/FavoriteRecipesFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import bellevuecollege.edu.cookpal.R
 import bellevuecollege.edu.cookpal.databinding.FragmentFavoriteRecipesBinding
+import bellevuecollege.edu.cookpal.network.Recipe
 import bellevuecollege.edu.cookpal.profile.UserProfile
 import bellevuecollege.edu.cookpal.profile.UserProfileHelper
+import bellevuecollege.edu.cookpal.recipe_parser.TAG
+import bellevuecollege.edu.cookpal.recipes.RecipeGridAdapter
 
 class FavoriteRecipesFragment : Fragment() {
 
     private lateinit var binding: FragmentFavoriteRecipesBinding
     private val up: UserProfile = UserProfile()
+
+    //private val _recipes = MutableLiveData<ArrayList<Recipe>>()
+    var favoriteRecipes: ArrayList<Recipe> = ArrayList()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +36,12 @@ class FavoriteRecipesFragment : Fragment() {
         //Load profile
         UserProfileHelper.loadProfile { data ->
             up.setProfile(data)
-            Log.d("FavoriteRecipeFragment Name", up.name)
-            Log.d("FavoriteRecipeFragment favorites", up.favoriteRecipes.toString())
+            favoriteRecipes = up.favoriteRecipes
+            Log.d(TAG, favoriteRecipes.toString())
         }
+
+
+
         return binding.root
     }
 
