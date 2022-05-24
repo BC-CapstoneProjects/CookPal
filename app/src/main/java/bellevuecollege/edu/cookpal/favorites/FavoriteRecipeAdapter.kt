e4ef815c116844b406d1bab47cb7c23cf8bcc285
TonnyAn <tonny.an@bellevuecollege.edu>
2022-05-23 19:36:31 -0700

    Added list_favorite_recipe.xml

--- app/src/main/java/bellevuecollege/edu/cookpal/favorites/FavoriteRecipeAdapter.kt
+++ app/src/main/java/bellevuecollege/edu/cookpal/favorites/FavoriteRecipeAdapter.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import bellevuecollege.edu.cookpal.R
-import bellevuecollege.edu.cookpal.databinding.FragmentFavoriteRecipesBinding
+import bellevuecollege.edu.cookpal.databinding.ListFavoriteRecipeBinding
 import bellevuecollege.edu.cookpal.recipes.Recipe
 
 class FavoriteRecipeAdapter(private val context: Context, private val favoriteRecipes: ArrayList<Recipe>) : BaseAdapter() {
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-    private lateinit var binding: FragmentFavoriteRecipesBinding
+
+    private lateinit var binding: ListFavoriteRecipeBinding
 
     override fun getCount(): Int {
         return favoriteRecipes.size
@@ -27,6 +29,13 @@ class FavoriteRecipeAdapter(private val context: Context, private val favoriteRe
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        // Get title element
+        val titleTextView = binding.recipeListTitle
+
+        // Get thumbnail element
+        val thumbnailImageView = binding.recipeListThumbnail
+
         return inflater.inflate(R.layout.fragment_favorite_recipes, parent, false)
     }
 
